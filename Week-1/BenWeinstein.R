20161f25b2e3f39a84b1d356e51277145fa0d1c2
bw4sz <benweinstein2010@gmail.com>
2014-01-30 18:32:28 -0500

    clarifying code.

--- Week-1/BenWeinstein.R
+++ Week-1/BenWeinstein.R
@@ -1,12 +1,15 @@
 #Seminar 1 - Bootstrap test
 require(ggplot2)
 
+#Repeat the experiment 1000 times
 global_means<-replicate(1000,mean(rnorm(100,sd=3)))
 hist(global_means)
 
+#make a dataframe
 draws<-data.frame(d=global_means)
 
-#100 replicates of bootstrap
+#1000 bootstrap replicates of 1 experiment
+j<-rnorm(100,sd=3)
 b<-replicate(1000,mean(sample(j,replace=TRUE)))
 
 boots<-data.frame(b=b)
