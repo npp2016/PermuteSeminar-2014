6020e8638d1c997ebcddc063acc1c5c8fa40dc20
wolfhagenj <jesse.wolfhagen@stonybrook.edu>
2014-01-30 18:21:16 -0500

    This is my Week 1 R code for the first project.

--- Week-1/JesseWolfhagen.R
+++ Week-1/JesseWolfhagen.R
@@ -1,1 +1,21 @@
-#
+##Draw 100 numbers from a normal distribution (mean = 1, sd = 3)
+#Get distribution of xbar through drawing from the distribution directly
+#Then get distribution of xbar through bootstrapping
+library(boot)
+library(bootstrap)
+study <- rnorm(100, mean = 1, sd = 3) #original data
+#Get distribution of direct draws
+directxbar <- c() #hold means
+for(i in 1:1000)
+{
+  directxbar <- c(directxbar, mean(rnorm(100, mean = 1, sd = 3)))
+}
+#Get distribution through bootstrap
+bsxbar <- c()
+for(i in 1:1000)
+{
+  bsxbar <- c(bsxbar, mean(sample(study, 100, replace = T)))
+}
+par(mfrow = c(1,2))
+hist(directxbar)
+hist(bsxbar)
