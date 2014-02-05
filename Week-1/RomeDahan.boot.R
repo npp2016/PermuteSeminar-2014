8a346e0821db03618fd50b3f3e6c02d040ec750f
Romain Dahan <dahanr@Romain-Dahans-MacBook-Pro.local>
2014-02-04 22:49:48 -0500

    Rome's Boot

--- Week-1/RomeDahan.boot.R
+++ Week-1/RomeDahan.boot.R
@@ -1,14 +1,15 @@
 #Rome Dahan's Bootstrap
+xbar<-c()
+for(i in 1:1000){
 x<-rnorm(100,1,sd=3)
-
-xbar<-mean(x)
-
+xbar[i]<-mean(x)
+}
+x<-rnorm(100,1,3)
 xbarstar<-c()
 for(i in 1:1000){
   xstar<-sample(x,100,replace=TRUE)
   xbarstar[i]<-mean(xstar)
 }
-xbar1<-mean(xbarstar)
 
-hist(x,col="red")
-hist(xbarstar)
+hist(xbar,col="red")
+hist(xbarstar,col="blue",add=TRUE)
