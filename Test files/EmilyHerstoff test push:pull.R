61175b4f62a00a61d2edc7e259a483e691cc5cc9
eherstoff <emily.herstoff@stonybrook.edu>
2014-02-06 16:44:53 -0500

    Testing...

--- Test files/EmilyHerstoff test push:pull.R
+++ Test files/EmilyHerstoff test push:pull.R
@@ -1,3 +1,7 @@
 ## This is Emily's push/pull test file.
 print("Emily's test file for GitHub")
 
+## Attempting to push and I get errors! Error messages below.
+## error: unable to read askpass response from 'rpostback-askpass'
+## fatal: could not read Username for 'https://github.com': Device not configured
+
