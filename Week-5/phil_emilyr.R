274da28c06d4430b7976c1b90058c4c9069bc71b
unknown <phil_mcdowall@btinternet.com>
2014-03-05 10:52:05 -0500

    formatting

--- Week-5/phil_emilyr.R
+++ Week-5/phil_emilyr.R
@@ -5,7 +5,7 @@ data<- read.csv("C:/R/Dolphin_data.csv", header=T)
 data<-data[,2:19]
 #make a backup of the data for later comparison
 datainit=data
-<<<<<<< HEAD
+
 #set chain length
 samps=5000
 
@@ -34,7 +34,7 @@ calculateHWI<-function(data){
     for(j in 1:18){
       Yb[i,j]=sum(data[,j]*(data[,i]!=1))
     }
-=======
+
 samps=500
 calculateHWI<-function(data){
 X=matrix(nrow=18,ncol=18)
@@ -59,7 +59,7 @@ Yb=matrix(nrow=18,ncol=18)
 for(i in 1:18){
   for(j in 1:18){
     Yb[i,j]=sum(data[,j]*(data[,i]!=1))
->>>>>>> c45fe29cb0b72116804441e9296a8ab57f6bfb4d
+
   }
   
   
@@ -96,11 +96,11 @@ switchMatrix<-function(data,changes){
   return(data)
 }
 
-<<<<<<< HEAD
-#initiate array to hold HWI's
-=======
 
->>>>>>> c45fe29cb0b72116804441e9296a8ab57f6bfb4d
+#initiate array to hold HWI's
+
+
+
 results=array(dim=c(18,18,samps))
 #initiate matrix to track how often an element has been swapped
 switched.elements=matrix(nrow=40,ncol=18,data=0)
@@ -143,14 +143,13 @@ saveGIF(interval = 0.01,ani.height = 600, ani.width = 3000, expr={par(mfrow=c(1,
 
 
 
-<<<<<<< HEAD
-=======
+
 S=vector()
 for(i in 1:samps){
   S[i]=sum((results[,,i]-meanHWI)^2/18)
 }
   
->>>>>>> c45fe29cb0b72116804441e9296a8ab57f6bfb4d
+
 sobs=sum((calculateHWI(datainit)-meanHWI)^2/18)
 
 sum(S>=sobs)/samps
