944903fc827d62f60b992e43eb2cba40bfcaad48
Sam Urmy <eloceanografo@gmail.com>
2014-03-05 23:56:49 -0500

    Get the whole dolphin-randomization shebang working

--- Week-5/sam_script.R
+++ Week-5/sam_script.R
@@ -1,14 +1,17 @@
 library(gdata)
+library(plyr)
 
-dolphins <- read.xls("Dolphin data.xlsx")
-dolphins <- as.matrix(dolphins[ , 2:ncol(dolphins)])
-
-dolphins
-image(dolphins)
+## Function definitions ########################################################
 
 hwi <- function(a, b, m) {
-  # m : co-occurrence matrix (individuals in columns, groups in rows)
-  # a, b : column numbers for a pair of individuals
+  # Calculate the half-weight index (HWI) between a pair of individuals over
+  # a set of encounters.
+  # 
+  # Arguments:
+  #   m : co-occurrence matrix (individuals in columns, groups in rows)
+  #   a, b : column numbers for a pair of individuals
+  # Value:
+  #   The (scalar) half-weight index
   both.present <- rowSums(m[ , c(a, b)]) == 2
   y.a  <- sum(m[! both.present , a])# encounters with only a
   y.b  <- sum(m[! both.present , b])# encounters with only b
@@ -16,10 +19,15 @@ hwi <- function(a, b, m) {
   return(x / (x + 0.5 * (y.a + y.b)))
 }
 
-hwi(2, 3, dolphins)
-
-
 swap <- function(m) {
+  # Searches an encounter-individual matrix randomly for a set of four cells on
+  # the corners of a rectangular sub-matrix that form a diagonal 2x2 matrix on
+  # their own, and swaps the diagonals.
+  #
+  # Arguments:
+  #   m : co-occurrence matrix (individuals in columns, groups in rows)
+  # Value:
+  #   The same matrix as supplied, but with the four cells swapped
   while (TRUE) {
     i <- sample.int(nrow(m), 2)
     j <- sample.int(ncol(m), 2)
@@ -31,13 +39,14 @@ swap <- function(m) {
   }
 }
 
-m <- dolphins
-m1 <- dolphins
-
-m1 <- swap(m1)
-image(m == m1)
-
 association.matrix <- function(m) {
+  # Calculates the HWI for each pair of individuals across all encounters.
+  #
+  # Arguments:
+  #   m : co-occurrence matrix (individuals in columns, groups in rows)
+  # Value:
+  #   A square association matrix, with the upper triangle filled with HWIs
+  #   between each pair of individuals.  The lower triangle is all zeros.
   D <- ncol(m)
   result <- matrix(0, D, D)
   for (i in 1:D) {
@@ -49,21 +58,63 @@ association.matrix <- function(m) {
 }
 
 S <- function(assoc.mat, e.mat) {
+  # Calculate the S-statistic from an association matrix and its expected value.
+  #
+  # Arguments:
+  #   assoc.mat : the association matrix
+  #   e.mat : the matrix of expexted HWIs
+  # Value:
+  #   The association matrix's S-statistic
   D <- nrow(assoc.mat)
-  sum(upper.tri(assoc.mat - e.mat)^2) / D^2
+  # don't have to worry about the upper triangle, since the
+  # lower one is all zeros
+  sum((assoc.mat - e.mat)^2) / D^2
 }
 
-D <- ncol(dolphins)
-n.swaps <- 100
-stack <- array(0, c(D, D, n.swaps))
+randomized.S <- function(m, n.swaps) {
+  # This function doesn't run any faster than the loop below, but uses a lot 
+  # less memory because it dosn't store all the intermediate association
+  # matrices.  Because it calculates the mean HWI matrix (e.matrix) on the fly,
+  # there needs to be a burn-in period for e.matrix to converge to its mean
+  D <- ncol(m)
+  e.matrix <- matrix(0, D, D)
+  S.trace <- rep(0, n.swaps)
+  
+  for (i in 1:n.swaps) {
+    o.matrix <-  association.matrix(m)
+    S.trace[i] <- S(o.matrix, e.matrix / i)
+    e.matrix  <- e.matrix + o.matrix
+    m <- swap(m)
+  }
+  return(S.trace)  
+}
+
+## Script ######################################################################
+
+dolphins <- read.xls("Dolphin data.xlsx")
+dolphins <- as.matrix(dolphins[ , 2:ncol(dolphins)])
+
+dolphins
+image(dolphins)
+
+image(dolphins == swap(dolphins))
+
 m <- dolphins
+D <- ncol(m)
+n.swaps <- 5000
+stack <- array(0, c(D, D, n.swaps))
 
 for (i in 1:n.swaps) {
   stack[ , , i] <- association.matrix(m)
   m <- swap(m)
 }
 
-e.matrix <- apply(stack, c(1,2), mean)
-image(e.matrix)
 
-apply(stack, 3, mean)
+e.matrix <- aaply(stack, c(1, 2), mean)
+S.trace <- aaply(stack, 3, S, e.mat=e.matrix)
+plot(S.trace, ty="l")
+
+acf(S.trace, 100)
+acf(diff(S.trace), 40)
+
+
