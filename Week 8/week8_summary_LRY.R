52af9770e134a38309d5280a8787c4cb04d69c45
Laurel Yohe <loloyohe@adm-129-49-192-108.wi-fi.stonybrook.edu>
2014-03-27 18:38:24 -0400

    commit

--- Week 8/week8_summary_LRY.R
+++ Week 8/week8_summary_LRY.R
@@ -1,6 +1,26 @@
 setwd("~/github_permute/PermuteSeminar-2014/Paulet Island")
 
+#read in the shape file 
 pts<-readShapePoints("AVIA seals.shp")
 
 lat<-pts$Lat
 long<-pts$Long
+
+#get the indices of lat long on the convex hull
+convex<-chull(lat, long)
+plot(lat[convex], long[convex])
+plot(lat[convex], long[convex], type = "l")
+#see the coordinates on the graph
+points(lat, long)
+
+
+
+min.lat<-min(lat)
+min.long<-min(long)
+max.lat<-max(lat)
+max.long<-max(long)
+
+pts.matrix<-matrix(c(lat,long), ncol = 2)
+pp <- as.ppp(pts.matrix, c(min.lat, max.lat, min.long, max.long))
+#calculate G-statistic
+g.stat<-Gest(pp)
