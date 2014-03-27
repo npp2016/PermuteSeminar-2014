c13b314bb8cc66488770fc30b082c420f547f4c4
eherstoff <emily.herstoff@stonybrook.edu>
2014-03-27 18:52:06 -0400

    working.

--- Week 8/Herstoff_Avia.R
+++ Week 8/Herstoff_Avia.R
@@ -1,6 +1,6 @@
 library(spatstat)
 library(maptools)
-beginner
+beginner ## getting help files for spatstat... 
 
 ## Avia Island seals
 setwd("~/PermuteSeminar-2014/Week 8/AVIA shapefile/")
@@ -8,10 +8,9 @@ setwd("~/PermuteSeminar-2014/Week 8/AVIA shapefile/")
 av <- readShapePoints("AVIA seals.shp")
 
 plot(av) ## plot all data
-
 plot(av$Lat, av$Long) ## plot by lat/long
-lat <- av$Lat
-long <- av$Long
+  lat <- av$Lat
+  long <- av$Long
 
 ## Convex hull; get indicies of this
 convex <- chull(lat, long)
@@ -20,9 +19,20 @@ points(lat, long) ## adds seals to the plot above so you can see where they are
 
 ## Calculate the G-stat using spatstat package
 help(spatstat)
+
 ## will need lat/long as matrix
 pts.matrix <- matrix(c(lat, long), ncol=2)
-pt.ppp <- as.ppp(pts.matrix, pt.matrix$window(3587))
-g.stat<- Gest(pt.ppp, )
 
+## finding the max/min of each lat/long point
+min.lat<-min(lat)
+min.long<-min(long)
+max.lat<-max(lat)
+max.long<-max(long)
+
+#calculate G-statistic
+pp <- as.ppp(pts.matrix, c(min.lat, max.lat, min.long, max.long))
+g.stat<- Gest(pp)
+## Check test output
+g.stat 
+plot(g.stat)
 
