0efca0ef968327f578a6c689fbe432a61b5d8dbf
Anusha Shankar <anusha.shankar@stonybrook.edu>
2014-04-03 20:02:36 -0400

    Adding Anusha's Week9 null model code

--- Null models/Week9_Anusha.R
+++ Null models/Week9_Anusha.R
@@ -14,10 +14,33 @@ head(humm[,1])
 
 ## Get richness per elevation
 humm$Richness <- rowSums(humm[,2:51])
-head(humm)
+
+## Tells you which rows each value starts at, and how many rows at a time
+rle(humm$Sp1)
 
 ## Plot and look at Richness vs Elevation
 plot(humm$Elevation, humm$Richness)
 
+## Sum number of elevation stations each species is present at
 humm[26,] <- colSums(humm)
-tail(humm)
+
+## Create matrix of 0s with same dimensions as the humm matrix
+newHumm <- matrix(0, ncol=51, nrow=25)
+
+newHumm[,1] <- humm[1:25,1]
+colnames(newHumm) <- colnames(humm[,1:51])
+
+## Calculate how many rows are available from the end of the col to start the 1s
+n <- length(humm$Elevation)
+ 
+## Loop to replace new matrix with 1s in different places along the column
+for(i in 2:51) {
+  TotalElev <- humm[n,i]
+  free <- (25 - TotalElev + 1)
+  x <- seq(1,free)
+  Row <- sample(x, 1)
+  y <- Row + TotalElev - 1
+  for(j in Row:y){
+    newHumm[j,i] <- 1  
+  }
+}
