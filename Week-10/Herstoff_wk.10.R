69b15de183e34af0025377363868ebc8ecfff06f
eherstoff <emily.herstoff@stonybrook.edu>
2014-04-10 18:49:40 -0400

    yay!

--- Week-10/Herstoff_wk.10.R
+++ Week-10/Herstoff_wk.10.R
@@ -63,17 +63,17 @@ NEprop<- c(.75,.15,.1)
 #	columns are sample sizes
 #	3rd dimension is replicates, of length [reps]
 
-output_array<- matrix(0,nrow=c(2,length(samps),reps))
+output_array<- array(data = 0, dim = c(2,length(samps), reps))
+## output_array(0,dim=c(2,length(samps),reps))
+
+## output_array<- matrix(data=0,nrow=dim(2,length(samps))),ncol=length(reps))
 
 tot.pool<- round(runif(reps,pool.range[1],pool.range[2]))	
 #randomly generate [reps]-length vector of total pool sizes
 
 
 #LOOP over replicates
-
 for (j in 1:reps){
-  print(j)	#progress output
-  
   #determine pool composition for replicate 
   natives <- paste("N",c(1:round(NEprop[1]*tot.pool[j])))	#list of native species  
   exotics <-paste("E",c(1:round(NEprop[2]*tot.pool[j])))	#list of exotic species
@@ -89,18 +89,16 @@ for (j in 1:reps){
   for(i in 2:length(allspp)) {				#loop over all remaining species    
     comm <- c(comm,rep(allspp[i],ind.vec[i]))		
     #populate remaining species with associated abundances   
-  }							#close loop
+  }		#close loop
   
-  
-  
+    
   #sample community using different sample sizes
   for(i in 1:length(samps)) {						
     #LOOP for different sample sizes
     rs <- sample(comm,samps[i])					#draw randomly from community pool of individuals with given sample size
     output_array[1,i,j] <- length(unique(rs[is.element(rs,natives)]))	#total richness of natives in the sample
-    output_array[2,i,j] <- length(unique(rs[is.element(rs,exotics)]))	#total richness of exotics in the sample
-    
-  }	#end sample size loop		
+    output_array[2,i,j] <- length(unique(rs[is.element(rs,exotics)]))	#total richness of exotics in the sample    
+  }	#end sample size loop	
   
 }		#end replicate loop
 
@@ -111,71 +109,51 @@ for (j in 1:reps){
 
 #Plot native-exotic richness relationship of largest sampling size
 plot(output_array[1,6,],output_array[2,6,],pch=".",xlab="native richness",ylab="exotic richness",
-     xlim=c(0,max(output[1:2,6,])),ylim=c(0,max(output[1:2,6,])))
-
-abline(lsfit(output[1,6,],output[2,6,]))		#least-squares regression line
-
+     xlim=c(0,max(output_array[1:2,6,])),ylim=c(0,max(output_array[1:2,6,])))
+abline(lsfit(output_array[1,6,],output_array[2,6,]))		#least-squares regression line
 
 
 #Plot native-exotic richness relationship of next smallest sampling size
-
-screen(2,F)
-
-plot(output[1,5,],output[2,5,],pch=".",xlab="native richness",ylab="exotic richness",xlim=c(0,samps[5]),ylim=c(0,samps[5]))
-
-abline(lsfit(output[1,5,],output[2,5,]))		#least-squares regression line
-
+plot(output_array[1,5,],output_array[2,5,],pch=".",xlab="native richness",ylab="exotic richness",
+     xlim=c(0,samps[5]),ylim=c(0,samps[5]))
+abline(lsfit(output_array[1,5,],output_array[2,5,]))		#least-squares regression line
 abline(samps[5],-1,lty=4)				#data constraint envelope, based on sample size
 
 
-
 #Plot native-exotic richness relationship of next smallest sampling size
-
-screen(3,F)
-
-plot(output[1,4,],output[2,4,],pch=".",xlab="native richness",ylab="exotic richness",xlim=c(0,samps[4]),ylim=c(0,samps[4]))
-
-abline(lsfit(output[1,4,],output[2,4,]))		#least-squares regression line
-
+plot(output_array[1,4,],output_array[2,4,],pch=".",xlab="native richness",ylab="exotic richness",
+     xlim=c(0,samps[4]),ylim=c(0,samps[4]))
+abline(lsfit(output_array[1,4,],output_array[2,4,]))		#least-squares regression line
 abline(samps[4],-1,lty=4)				#data constraint envelope, based on sample size
 
 
-
 #Plot native-exotic richness relationship of next smallest sampling size
-
-screen(4,F)
-
-plot(jitter(output[1,3,]),jitter(output[2,3,]),pch=".",xlab="native richness",ylab="exotic richness",xlim=c(0,samps[3]),ylim=c(0,samps[3]))
-
-abline(lsfit(output[1,3,],output[2,3,]))		#least-squares regression line
-
+plot(jitter(output_array[1,3,]),jitter(output_array[2,3,]),pch=".",xlab="native richness",ylab="exotic richness",
+     xlim=c(0,samps[3]),ylim=c(0,samps[3]))
+abline(lsfit(output_array[1,3,],output_array[2,3,]))		#least-squares regression line
 abline(samps[3],-1,lty=4)				#data constraint envelope, based on sample size
 
 
-
 #Plot native-exotic richness relationship of next smallest sampling size
-
-screen(5,F)
-
-plot(jitter(output[1,2,]),jitter(output[2,2,]),pch=".",xlab="native richness",ylab="exotic richness",xlim=c(0,samps[2]),ylim=c(0,samps[2]))
-
-abline(lsfit(output[1,2,],output[2,2,]))		#least-squares regression line
-
+plot(jitter(output_array[1,2,]),jitter(output_array[2,2,]),pch=".",xlab="native richness",ylab="exotic richness",
+     xlim=c(0,samps[2]),ylim=c(0,samps[2]))
+abline(lsfit(output_array[1,2,],output_array[2,2,]))		#least-squares regression line
 abline(samps[2],-1,lty=4)				#data constraint envelope, based on sample size
 
 
-
 #Plot native-exotic richness relationship of next smallest sampling size
-
-screen(6,F)
-
-plot(jitter(output[1,1,]),jitter(output[2,1,]),pch=".",xlab="native richness",ylab="exotic richness",xlim=c(0,samps[1]),ylim=c(0,samps[1]))
-
-abline(lsfit(output[1,1,],output[2,1,]))		#least-squares regression line
-
+plot(jitter(output_array[1,1,]),jitter(output_array[2,1,]),pch=".",xlab="native richness",ylab="exotic richness",
+     xlim=c(0,samps[1]),ylim=c(0,samps[1]))
+abline(lsfit(output_array[1,1,],output_array[2,1,]))		#least-squares regression line
 abline(samps[1],-1,lty=4)				#data constraint envelope, based on sample size
 
 ######
+#####=========================================================================================
+
+
+
+
+############## Now playing with Emily I's dataset ############################################
 
 
 
