b173a5a4c79b80d1053fccdc97bacec219b32b7f
eherstoff <emily.herstoff@stonybrook.edu>
2014-04-10 17:55:58 -0400

    wk 10

---
+++ Week-10/Herstoff_wk.10.R
@@ -0,0 +1,9 @@
+## null models and spatial distributions
+## read in Emily's data from github
+setwd("Week-10/")
+ sp.ct <-   read.csv(file="speciescounts.csv")
+ site.gr <- read.csv(file="site_groupings.csv")
+
+
+
+
