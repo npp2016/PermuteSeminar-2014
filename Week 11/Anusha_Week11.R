84ea2ed948f8bc9b8fca4ffe00f333ecaea0c416
Anusha Shankar <anusha.shankar@stonybrook.edu>
2014-04-17 19:01:03 -0400

    pushing updated code. It's very tiny.

--- Week 11/Anusha_Week11.R
+++ Week 11/Anusha_Week11.R
@@ -1,4 +1,12 @@
+require(vegan)
+require(devtools)
+require(ggbiplot)
+## installed "ggbiplot", "vqv"
+
 setwd("C:\\Users\\Anusha\\Documents\\GitHub\\PermuteSeminar-2014\\Week 11")
 
-paleodata <- read.csv("epipalassemblages.csv")
+paleodata <- read.csv("epipalassemblages.csv", row.names=1)
 
+pca.paleo <- prcomp(paleodata[,1:31])
+
+ggbiplot(pca.paleo,labels=rownames(paleodata)) + theme_bw()
