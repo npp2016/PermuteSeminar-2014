9a6118483156dd2211443edd05a287266c67bc73
eherstoff <emily.herstoff@stonybrook.edu>
2014-05-01 19:13:26 -0400

    commit

--- Week-13/Herstoff.R
+++ Week-13/Herstoff.R
@@ -1,14 +1,33 @@
 library(reshape2)
 library(ggplot2)
 library(vegan)
+
+###########################################################################
+
 ## Trying again... uploading new data (tweaked in Excel)
 desmids <-read.csv("Week-13/Spp by Swamp vs. NaCihadle.csv")
   desmids[1]<-NULL
   desmids[1]<-NULL ## removing col 1 and 2 (words) so only numbers (for vegdist)
     head(desmids)
-des.dist <-vegdist(desmids, binary=T)
+des.dist <-vegdist(desmids, binary=F)
 
 ###########################################################################
+
+## Pirated from Laurel- trying to to see how stuff works.
+
+setwd("/Users/emilypetchler/Documents/GitHub/PermuteSeminar-2014.test/Week-13/")
+
+data<-read.csv("Svoboda_Supp.T2.csv") ## Data from supp figure on species
+  data<-na.omit(data)
+  data<-data[,-1] ## get rid of species names
+  data<-t(data)   ## transpose the data.
+
+environment<-read.csv("Svoboda_T2_csv.csv", row.names=1) ## abiotic conditions
+  environment$habitat<-0
+  environment$habitat[1:10]<-"swamp"
+  environment$habitat[11:20]<-"bog"
+environment<-environment[-1,]
+environment<-environment[-10,]
 
 #between two sites
 distance<-vegdist(data, binary = TRUE)
@@ -20,28 +39,3 @@ M<-(length(data$Taxon)*(length(data$Taxon)-1))/2
 
 #between sites of teh swamps
 
-
-###########################################################################
-
-library(vegan)
-??anosim ## Analysis of Similarities
-??vegdist ## Dissimilarity Indices for Community Ecologists
-
-## Using vegdist to calculate dissimilarity indicies
-desmids.t <- t(desmids) ##transpose desmid data
-## Rename row/columns so they're numeric (for vegdist)
-desmids.t[2,]<-1
-
-
-desmid.dist<-vegdist(desmids.t)
-vegdist(x=desmids, method="bray", binary=FALSE, diag=FALSE, upper=FALSE, na.rm = FALSE) 
-
-
-
-data(dune)
-data(dune.env)
-dune.dist <- vegdist(dune)
-attach(dune.env)
-dune.ano <- anosim(dune.dist, Management)
-summary(dune.ano)
-plot(dune.ano)
