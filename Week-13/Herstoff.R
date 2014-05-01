4651215925cb6bbcefebf8e4493fceed35a032d7
eherstoff <emily.herstoff@stonybrook.edu>
2014-05-01 18:43:53 -0400

    commit

--- Week-13/Herstoff.R
+++ Week-13/Herstoff.R
@@ -3,14 +3,23 @@ library(ggplot2)
 
 ## Seeing how Sam's melt works
 desmids <- read.csv("Week-13/Svoboda_Supp.T2.csv")
-desmids.m <- melt(desmids) ## Using Taxon as id variables
-  head(desmids.m)
-desmids.m <- cbind(desmids.m, 
-                   colsplit(desmids.m$variable, "_", c("ecosystem", "sitepool")))
+desmids.me <- melt(desmids) ## Using Taxon as id variables
+  head(desmids.me)
+desmids.me <- cbind(desmids.me, 
+                   colsplit(desmids.me$variable, "_", c("ecosystem")))
 
-desmids.m$site <- substr(as.character(desmids.m$sitepool), 1, 1)
-desmids.m$pool <- substr(as.character(desmids.m$sitepool), 3, 3)
+desmids.me$site <- substr(as.character(desmids.me$sitepool), 1, 1)
+desmids.me$pool <- substr(as.character(desmids.m$sitepool), 3, 3)
 desmids.m <- desmids.m[c("Taxon", "value", "ecosystem", "site", "pool")]
 
 head(desmids.m)
 write.csv(desmids.m, "Svoboda_supp_T2_longform.csv")
+
+
+library(vegan)
+??anosim ## Analysis of Similarities
+?? vegdist ## Dissimilarity Indices for Community Ecologists
+
+## Using vegdist to calculate dissimilarity indicies
+vegdist(x=desmids, method="bray", binary=FALSE, diag=FALSE, upper=FALSE, na.rm = FALSE) 
+
