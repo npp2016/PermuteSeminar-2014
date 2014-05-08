7f5b78c2d0d646c4d8c6f736a0efe9ba686a3cf6
eherstoff <emily.herstoff@stonybrook.edu>
2014-05-08 17:54:00 -0400

    commit

---
+++ Week 14/Herstoff.R
@@ -0,0 +1,23 @@
+library(picante)
+require(picante)
+setwd("~/PermuteSeminar-2014/Week 14")
+
+hum <- read.tree("hum294.tre") ## read in phylogenetic tree
+  str(hum) ## to see structure of the new class of things
+hum$tip.label ## show tip labels for tree
+cophenetic.phylo(hum) 
+  ## cophenetic score = pairwise matrix of relatedness of tips to most recent common ancestor node
+  ## more related, 0; less related= higher#s.
+  ## metric of relatedness.
+hist(cophenetic.phylo(hum))
+  ## hist the cophenetic score/relatedness... seems R-skew (lots of unrelated spp)
+
+##The other two data sets for today:
+sites <-read.csv('Sites.csv')
+siteXspp <- read.csv("SiteXspp.csv")
+
+colnames(siteXspp)
+  ## reformat the strings so hum and siteXspp are same thing.
+strsplit(colnames(siteXspp),"//.")
+strsplit((hum$tip.label),"//.")
+
